76d6dd976ced464ee99ed89b2cb58db579a081e9
CoinSniperDev <dev@coinsniper.io>
2025-01-22 18:15:00 -0800

    Add minification

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import App from './App';
 import ReactGA from 'react-ga4';
 import './index.css'; // optional global styles
 
+ReactGA.initialize('G-1CK1JC1EQ5');
 // TODO: Remove debug here
-// ReactGA.initialize('G-1CK1JC1EQ5');
+/*
 ReactGA.initialize('G-1CK1JC1EQ5', {
   gaOptions: {
     debug_mode: true,
@@ -13,7 +14,7 @@ ReactGA.initialize('G-1CK1JC1EQ5', {
   gtagOptions: {
     debug_mode: true,
   },
-});
+}); */
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
